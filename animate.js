62450d59c2b42dc5c26f6ca344d1dd2b6a940aa2
mattzandt <mattzandt@gmail.com>
2015-11-06 00:58:53 -0600

    Port to GSAP complete. Dynamically change size on window resize complete

--- animate.js
+++ animate.js
@@ -1,59 +1,46 @@
 $(function (){
+  // Load world.png
+  var planet = document.getElementById("planet");
 
-	// remove scroll options
-	document.documentElement.style.overflow = 'hidden';  // firefox, chrome
-  document.body.scroll = "no"; // ie only
+  // scale background image relative to window size
+  $(document.body).css('background-size' ,(512 * window.innerWidth / (window.innerWidth + 512)) + 'px');
 
-	// Load world.png
-	collie.ImageManager.add({
-		"planet" : "images/world.png",
-		"stars" : "images/stars-64x64.png" //temporary background
-	});
+  // initialize planet animation
+  TweenMax.to(planet, 0, {
+    //set position to center of screen
+    left: (window.innerWidth / 2) - 256,
+    top: (window.innerHeight / 2) - 256,
 
-	//Create a world layer
-	var worldLayer = new collie.Layer({
-		width: window.screen.availWidth,
-		height: window.screen.availHeight
-	});
+    // scale planet relative to screen
+    scaleX: window.innerWidth / (window.innerWidth + 512),
+    scaleY: window.innerWidth / (window.innerWidth + 512),
 
-	//Create star background to display
-	var starBackground = new collie.DisplayObject({
-		originX: 0,
-		originY: 0,
-		width: window.screen.availWidth * 3,
-		height: window.screen.availHeight * 3,
-		scaleX: window.innerWidth / (window.innerWidth + 512),
-		scaleY: window.innerWidth / (window.innerWidth + 512),
-		backgroundRepeat: "repeat",
-		backgroundImage: "stars"
-	}).addTo(worldLayer);
+    // bring planet to the front of window
+    zIndex: 3
+    });
 
-	//Create world object to display
-	var world = new collie.DisplayObject({
-		x: (window.innerWidth / 2) - 256, //center planet on x-axis
-		y: (window.innerHeight / 2) - 256, //center planet on y-axis
-		velocityRotate : 2,
-		scaleX: window.innerWidth / (window.innerWidth + 512),
-		scaleY: window.innerWidth / (window.innerWidth + 512),
-		backgroundImage : "planet"
-	}).addTo(worldLayer);
+  // start rotating planet
+  TweenMax.to(planet, 100, {
+    rotation:"360",
+    repeat: -1,
+    ease:Linear.easeNone
+  });
 
-	//Add worldLayer to Renderer
-	collie.Renderer.addLayer(worldLayer);
+  $(window).resize(function(){
 
-	// Load Renderer into container id
-	collie.Renderer.load(document.getElementById("world"));
+    // adjust planet to new screen size
+    TweenMax.to(planet, 0, {
+      //set position to center of screen
+      left: (window.innerWidth / 2) - 256,
+      top: (window.innerHeight / 2) - 256,
 
-	//Start rendering
-	collie.Renderer.start();
+      // scale planet relative to screen
+      scaleX: window.innerWidth / (window.innerWidth + 512),
+      scaleY: window.innerWidth / (window.innerWidth + 512)
+    });
 
-	// Dynamically change planet location and scale on window resize
-	$(window).resize(function(){
-		world.set("x", (window.innerWidth / 2) - 256);
-		world.set("y", (window.innerHeight / 2) - 256);
-		world.set("scaleX", window.innerWidth / (window.innerWidth + 512));
-  	world.set("scaleY", window.innerWidth / (window.innerWidth + 512));
-		starBackground.set("scaleX", window.innerWidth / (window.innerWidth + 512));
-		starBackground.set("scaleY", window.innerWidth / (window.innerWidth + 512));
-	});
+    // adjust background to new screen size
+    $(document.body).css('background-size' ,(512 * window.innerWidth / (window.innerWidth + 512)) + 'px');
+  });
+
 });
