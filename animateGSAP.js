97ab72b09d44e3034ebcdec0d02e3e2a49d3dfa4
mattzandt <mattzandt@gmail.com>
2015-11-05 19:00:04 -0600

    Begin transition to GSAP. Planet completely ported

---
+++ animateGSAP.js
@@ -0,0 +1,25 @@
+$(function (){
+  // Load world.png
+  var planet = document.getElementById("planet");
+
+  // initialize planet animation
+  TweenMax.to(planet, 0, {
+    //set position to center of screen
+    left: (window.innerWidth / 2) - 256,
+    top: (window.innerHeight / 2) - 256,
+
+    // scale planet relative to screen
+    scaleX: window.innerWidth / (window.innerWidth + 512),
+    scaleY: window.innerWidth / (window.innerWidth + 512)
+    });
+
+  // start rotating planet
+  TweenMax.to(planet, 100, {
+    rotation:"360",
+    repeat: -1,
+    ease:Linear.easeNone
+  });
+
+  
+
+});
